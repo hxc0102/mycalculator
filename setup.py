857811c6f97b11a83a066d6e83dbda759b4138a2
hxc0102 <gur345@naver.com>
2022-05-18 09:45:01 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 
 setuptools.setup (
   include_package_data = True,
-  name='seokcalculator001',
+  name='mycalculator119',
   version='0.0.1',
-  description='oss-dev seokcalculator001',
-  author='seokjinkang',
-  author_email='2615208@naver.com',
-  url = 'https://github.com/seokko/mycalc',
+  description='oss-dev mycalculator119',
+  author='hxc.oh',
+  author_email='gur345@naver.com',
+  url = 'https://github.com/hxc0102/mycalculator',
   install_requires=['pytest'],
-  long_description = 'oss-dev mycalc',
+  long_description = 'my calculator python module',
   long_description_content_type = 'text/markdown',
   classifiers=[
     "Programming Language :: Python :: 3",
